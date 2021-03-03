9a1290f1794d2f9e317133ca0c00b7217f07ec80
Gordon Chen <gordon890908@gmail.com>
2021-03-04 00:04:28 +0800

    Initialize code

---
+++ api/app.js
@@ -0,0 +1,83 @@
+const path = require('path')
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const morgan = require('morgan')
+const session = require('express-session')
+const MongoStore = require('connect-mongo')(session)
+const connectDB = require('./config/db')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const User = require('./models/User')
+
+// Load config
+dotenv.config({ path: './config/config.env' })
+
+connectDB()
+
+// EXPRESS
+const app = express()
+
+// BODYPARSER
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// CORS
+app.use(cors());
+
+// Logging
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+
+// Sessions
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
+  })
+)
+
+// Routing
+const { OAuth2Client } = require('google-auth-library')
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID)
+
+// @desc    Check DB if the user exist, if not, add user into the DB
+// @route   POST /auth/google
+app.post("/auth/google", async (req, res) => {
+  const { token }  = req.body
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: process.env.GOOGLE_CLIENT_ID
+    });
+    const newUser = {
+      googleId: ticket.getPayload().sub,
+      displayName: ticket.getPayload().name,
+      email: ticket.getPayload().email,
+      image: ticket.getPayload().picture
+    }
+    try {
+      let user = await User.findOne({ googleId: ticket.getPayload().sub })
+
+      if (user) {
+        console.log("There is already this user in the db")
+      } else {
+          user = await User.create(newUser)
+          console.log("Creating a new user in the db")
+      }
+
+    } catch (err) {
+      console.error(err)
+    }
+
+    res.sendStatus(200)
+})
+
+const PORT = process.env.PORT || 9000
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+)
---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import GoogleLogin from 'react-google-login';
+//import axios from 'axios';
+
+function App() {
+  const responseSuccessGoogle = async (googleData) => {
+    console.log(googleData.tokenId);
+    await fetch("/auth/google", {
+      method: "POST",
+      body: JSON.stringify({ 
+        token: googleData.tokenId
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }).then(res => {
+      if (res.ok) {
+        console.log("Google signin success")
+      } else {
+        console.log("Google signin fail")
+      }
+    })
+  };
+
+  const responseFailureGoogle = (res) => {
+    console.log(res);
+  };
+
+  return (
+    <div>
+      <div className="col-md-6 offset-md-3 text-center">
+        <h1>Login with GOOGLE</h1>
+          <GoogleLogin
+            clientId="281834657367-r6h4r3t5unmp2q9rl9usg3gp9k23iq3b.apps.googleusercontent.com"
+            buttonText="Login"
+            onSuccess={responseSuccessGoogle}
+            onFailure={responseFailureGoogle}
+            cookiePolicy={'single_host_origin'}
+          />
+      </div>
+    </div>
+  );
+}
+
+export default App;
