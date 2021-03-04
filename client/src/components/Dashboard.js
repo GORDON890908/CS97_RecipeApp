cd60d5f6a6e5f4c2177fe6c54862ad00fb430fca
Gordon Chen <gordon890908@gmail.com>
2021-03-04 23:35:40 +0800

    Remove unnessary files

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,13 +2,15 @@ import React from 'react'
 
 // The Header creates links that can be used to navigate
 // between routes.
-const googleId = localStorage.getItem('googleId');
+function Dashboard() {
+    const googleId = localStorage.getItem('googleId');
 
-const Header = () => (
-    <div>
-        <header>Dashboard</header>
-        <p> {googleId} </p>
-    </div>
-)
+    return (
+        <div>
+            <header>Dashboard</header>
+            <p> {googleId} </p>
+        </div>
+    )
+}
 
-export default Header;
+export default Dashboard;
