94a8868dec5581e51c1831e3544212725aba3fec
Dylan Sun <zhenrong003@g.ucla.edu>
2021-03-09 01:19:51 +0800

    testing

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -3,34 +3,34 @@ import React, { useState } from 'react';
 function Recipe() {
     const [recipe, setRecipe] = useState(null);
 
-    const fetchRecipe = async() => {
+    const fetchRecipe = async () => {
         await fetch(`/recipe${window.location.search}`)
-        .then(res => res.json())
-        .then(data => {
-            setRecipe(data);
-        });
+            .then(res => res.json())
+            .then(data => {
+                setRecipe(data);
+            });
     }
 
     return (
         <div>
-        <div className="text-center">
-            <h1>Recipe page</h1>
-            <div>
-                {recipe && recipe.map((recipe, index) => {
-                    console.log(recipe)
-                    return (
-                        <div key = {index}>
-                            <p>Recipe Name: {recipe.recipeName}</p>
-                            <p>Description: {recipe.description}</p>
-                        </div>
-                    )
-                })}
+            <div className="text-center">
+                <h1>Recipe page</h1>
+                <div>
+                    {recipe && recipe.map((recipe, index) => {
+                        console.log(recipe)
+                        return (
+                            <div key={index}>
+                                <p>Recipe Name: {recipe.recipeName}</p>
+                                <p>Description: {recipe.description}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+                <button onClick={fetchRecipe}> fetch </button>
+                <div>
+                    Commectingd adlakdljljljkasd
             </div>
-            <button onClick={fetchRecipe}> fetch </button>
-            <div>
-                Commecting
             </div>
-        </div>
         </div>
     );
 }
