c661e4485c492c76db2bd4393d0f840e6dbc5c26
Dylan Sun <zhenrong003@g.ucla.edu>
2021-03-09 13:10:35 +0800

    add new component for detail page

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import Comment from "./Comment.js"
 function Recipe() {
     const [recipe, setRecipe] = useState(null);
 
@@ -8,30 +8,54 @@ function Recipe() {
             .then(res => res.json())
             .then(data => {
                 setRecipe(data);
+                console.log(data)
             });
     }
 
     return (
         <div>
-            <div className="text-center">
-                <h1>Recipe page</h1>
-                <div>
-                    {recipe && recipe.map((recipe, index) => {
-                        console.log(recipe)
-                        return (
-                            <div key={index}>
-                                <p>Recipe Name: {recipe.recipeName}</p>
-                                <p>Description: {recipe.description}</p>
-                            </div>
-                        )
-                    })}
-                </div>
-                <button onClick={fetchRecipe}> fetch </button>
-                <div>
-                    Commectingd adlakdljljljkasd
+            <div className="recipeDetail">
+                <h1>title of the recipe`</h1>
+                <img src="https://www.w3schools.com/images/picture.jpg" alt="apple" />
+                <ol>
+                    <li>buy apple</li>
+                    <li>cut apple</li>
+                    <li>eat it</li>
+                    {/* <ol>
+            {ingredients.map(ingredient => (
+                <li>{ingredient.text}</li>
+            ))}
+            </ol> */}
+                </ol>
+
             </div>
-            </div>
+            <div style={{ margin: '10px', paddingLeft: '25px' }}><LikeDislikes></LikeDislikes></div>
+            {/* actions={<LikeDislikes> //recipe recipeId={recipeId} userId={localStorage.getItem('userId')} />} */}
+            <Comment CommentList={CommentList} postId={Recipe.id} refreshFunction={updateComment} />
+
         </div>
+
+
+        // <div>
+        //     <div className="text-center">
+        //         <h1>Recipe page</h1>
+        //         <div>
+        //             {recipe && recipe.map((recipe, index) => {
+        //                 console.log(recipe)
+        //                 return (
+        //                     <div key={index}>
+        //                         <p>Recipe Name: {recipe.recipeName}</p>
+        //                         <p>Description: {recipe.description}</p>
+        //                     </div>
+        //                 )
+        //             })}
+        //         </div>
+        //         <button onClick={fetchRecipe}> fetch </button>
+        //         <div>
+        //             Commectingd adlakdljljljkasd
+        //     </div>
+        //     </div>
+        // </div>
     );
 }
 
