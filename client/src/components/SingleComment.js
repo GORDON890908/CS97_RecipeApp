a582b94f14083ecf52c9a9dd45767d38a836ea30
Dylan Sun <zhenrong003@g.ucla.edu>
2021-03-09 13:15:21 +0800

    update package for featureimport

---
+++ client/src/components/SingleComment.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const { TextArea } = Input;
+
+function SingleComment(props) {
+
+    console.log(props.comment)
+    // const action = 
+    return (
+        <div>
+            <Comment className="card"
+                // action={action}
+                avatar={
+                    <img className="image" src={"https://www.w3schools.com/images/picture.jpg"} alt="image" />
+                    //props.comment.writer.image} alt="image" />
+                }
+                author={<h2 className="header">{props.comment.writer}</h2>}
+
+                content={<div class="container">{props.comment.content} </div>}
+            >
+            </Comment>
+        </div >
+
+    )
+}
+
+export default SingleComment;
