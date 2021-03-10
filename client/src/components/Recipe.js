9f404ab8e8139297b49a94cea48506f009781319
Gordon Chen <gordon890908@gmail.com>
2021-03-10 11:00:11 +0800

    Add comment section

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -1,22 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Comment from "./Comment.js"
 import style from "../css/detailPage.css";
-import LikeDislikes from "./LikeDislike.js"
 
 function Recipe() {
     const [recipe, setRecipe] = useState(null);
     const [comment, setComment] = useState("");
     const [commentList, setCommentList] = useState(null);
-
-    const recipeDetail = {
-        recipeName: "",
-        createdAt: "",
-        ingredient: {},
-        procedures: {},
-        review: [],
-        userName: "",
-        id: "",
-    }
 
     const fetchRecipe = async () => {
         await fetch(`/recipe${window.location.search}`)
@@ -26,40 +15,11 @@ function Recipe() {
             console.log(data)
         });
     }
-  
-    const handleCommentChange = (event) => {
-        setComment(event.target.value);
-    }
     
     const updateComment = (newComment) => {
         console.log(newComment)
-        setCommentList(CommentList.concat(newComment));
+        setCommentList(commentList.concat(newComment));
     }
-    
-    // /////////////////////////Need to change this part to Comment.js/////////////////////////
-    const handleComment = async() => {
-        const info = {
-            userName: localStorage.name,
-            message: comment,
-            recipeID: window.location.search.substring(4),
-        }
-        try{
-            await fetch("/comment", {
-                method: "POST",
-                body: JSON.stringify(info),
-                headers: {
-                "Content-Type": "application/json",
-                }
-                }).then(res => {
-                if (res.ok) {
-                    console.log("Comment Success")
-                }
-                })
-        }catch(err){
-            console.log("Comment Fail");
-        }
-    }
-    ////////////////////////////////////////////////////////////////////////////////////////////
 
     const fetchComment = async() => {
         await fetch(`/comment${window.location.search}`)
@@ -72,7 +32,7 @@ function Recipe() {
     useEffect(()=>{
         fetchRecipe();
         fetchComment();
-      }, () => handleComment)
+    }, [])
 
     return (
 
@@ -83,10 +43,9 @@ function Recipe() {
                         <div key={index} className="recipeDetail">
                             <h2 style={{ margin: "10px" }}>{(recipe.recipeName)} </h2>
                             <p style={{ paddingLeft: "10px" }}>Description: {recipe.description}</p>
-                            {/* <img src="https://v1.nitrocdn.com/KQYMGOLIdXGmoAcyJsPOrQDKktgCbwtG/assets/static/optimized/rev-a960d32/wp-content/uploads/2016/03/Pressure-Cooker-Curry-IV.jpg" alt="apple" width="100%" height="50%" /> */}
                             <ol style={{ margin: "10px" }}>
-                                {(recipe.ingredients.map(ingredient => (
-                                    <li>{ingredient}</li>
+                                {(recipe.ingredients.map((ingredient, index) => (
+                                    <li key={index}>{ingredient}</li>
                                 )))}
                             </ol>
                             <p style={{ margin: "10px" }}>{recipe.procedures}</p>
@@ -94,11 +53,7 @@ function Recipe() {
                     )
                 })}
             </div>
-
-            <div style={{ paddingLeft: '450px' }}><LikeDislikes></LikeDislikes></div>
-            {/* actions={<LikeDislikes> //recipe recipeId={recipeId} userId={localStorage.getItem('userId')} />} */}
-            <Comment CommentList={CommentList} postId={Recipe.id} refreshFunction={updateComment} />
-
+            <Comment CommentList={commentList}/>
         </div >
     );
 }
