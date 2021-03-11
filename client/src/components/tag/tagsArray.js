41cd4bbf813048c18b3f241a24f4a2d3b355cbe1
Gordon Chen <gordon890908@gmail.com>
2021-03-11 16:00:21 +0800

    Recipe page formating

--- client/src/components/tag/tagsArray.js
+++ client/src/components/tag/tagsArray.js
@@ -1,6 +1,4 @@
 export const getTagsArray = ()=> ([
     {id: 'African', name: "African"}, {id: 'Chinese', name: "Chinese"}, {id: 'American', name: "American"}, 
-    {id: 'German', name: "German"}, {id: 'Indonesian', name: "Indonesian"}, {id: 'Japanese', name: "Japanese"}, 
-    {id: 'Mexican', name: "Mexican"}, {id: 'Italian', name: "Italian"}, {id: 'Thai', name: "Thai"}, 
+    {id: 'German', name: "German"}, {id: 'Mexican', name: "Mexican"}
 ])
-
